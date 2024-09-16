05316e2ea27dba6511065ca32c4b8aee0fe2ce4e
Albert Lanza <albert2000_lanza@hotmail.com>
2024-09-16 12:22:30 +0200

    Header component started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import CodeEditor from '@/components/CodeEditor'
+// import CodeEditor from '@/components/CodeEditor'
+import Header from '@/sections/Header'
 
 export default function Home() {
   return (
     <>
-      <h1 className='text-9xl text-center'>{'<FixItNow />'}</h1>
-      <CodeEditor />
+      <Header />
+      {/* <CodeEditor /> */}
     </>
   )
 }
---
+++ src/sections/Header.tsx
@@ -0,0 +1,27 @@
+import RrssLayout from '@/icons/RrssLayout'
+import Image from 'next/image'
+import user from '@/assets/imgs/user.webp'
+
+const Header = () => {
+  return (
+    <header className='grid grid-cols-[1fr,auto,1fr] items-center justify-center py-4 px-8'>
+      <div>
+        <RrssLayout />
+      </div>
+
+      <h1 className='text-9xl text-center'>{'<FixItNow />'}</h1>
+
+      <div className='justify-self-end'>
+        <Image
+          src={user}
+          alt='user'
+          width={160}
+          height={160}
+          className='size-[100px] rounded-full'
+        />
+      </div>
+    </header>
+  )
+}
+
+export default Header
