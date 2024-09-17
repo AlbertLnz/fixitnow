36ce5577e425947603f3f9ba4c7a06e16dbf9f0b
Albert Lanza <albert2000_lanza@hotmail.com>
2024-09-17 14:12:32 +0200

    Show the themes list

--- src/components/ExampleEditor.tsx
+++ src/components/ExampleEditor.tsx
@@ -4,11 +4,15 @@ import { themes } from '@/data/themes'
 import { Editor } from '@monaco-editor/react'
 import * as monaco from 'monaco-editor'
 import { useMonacoEditorStore } from '@/store/store'
+import { useState } from 'react'
 
 const palette_colors = ['bg-orange-400', 'bg-green-500', 'bg-violet-600']
+const themeList = themes.map((theme) => theme.name)
 
 const ExampleEditor = () => {
   const { setEditorRef } = useMonacoEditorStore()
+
+  const [themeSelected, setThemeSelected] = useState(themeList[0])
 
   const handleEditorDidMount = (monacoInstance: typeof monaco) => {
     // Example of 'OneDarkPro' theme
@@ -20,6 +24,22 @@ const ExampleEditor = () => {
     })
   }
 
+  const prev = () => {
+    setThemeSelected((prevTheme) => {
+      const currIndex = themeList.indexOf(prevTheme)
+      const newIndex = currIndex === 0 ? themeList.length - 1 : currIndex - 1
+      return themeList[newIndex]
+    })
+  }
+
+  const next = () => {
+    setThemeSelected((prevTheme) => {
+      const currIndex = themeList.indexOf(prevTheme)
+      const newIndex = currIndex === themeList.length - 1 ? 0 : currIndex + 1
+      return themeList[newIndex]
+    })
+  }
+
   const onMount = (editor: monaco.editor.IStandaloneCodeEditor) => {
     setEditorRef(editor)
     editor.focus()
@@ -28,9 +48,9 @@ const ExampleEditor = () => {
   return (
     <div className='relative w-[80%] h-full bg-stone-800 flex flex-col items-center justify-center place-content-center mx-auto overflow-hidden'>
       <div className='top-0 mt-2 flex justify-around w-full'>
-        <button>PREV</button>
-        <h4 className='text-4xl'>Dracula</h4>
-        <button>NEXT</button>
+        <button onClick={prev}>Prev</button>
+        <h4 className='text-4xl'>{themeSelected}</h4>
+        <button onClick={next}>Next</button>
       </div>
       <div className='bg-red-400 w-full flex-1 flex justify-between'>
         <div className='w-[85%] bg-green-400'>
