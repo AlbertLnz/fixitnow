6a2a91289de8c80367ff48e1be644217a52a217c
Albert Lanza <albert2000_lanza@hotmail.com>
2024-09-18 11:47:37 +0200

    Supabase installed & test it (works fine)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
-const page = () => {
-  return <div>page</div>
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+const HomePage = async () => {
+  const supabase = createServerComponentClient({ cookies })
+  const { data: user } = await supabase.from('users').select('*').eq('id', 1)
+
+  return (
+    <div>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+    </div>
+  )
 }
 
-export default page
+export default HomePage
